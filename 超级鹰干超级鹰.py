42cd3101e167b916ccad71deb1092447f83855cc
Ttanxuan <138363538+Ttanxuan@users.noreply.github.com>
2023-08-13 14:59:50 +0800

    Update 超级鹰干超级鹰.py

--- 超级鹰干超级鹰.py
+++ 超级鹰干超级鹰.py
@@ -14,7 +14,7 @@ time.sleep(0.5)
 
 # 获得验证码图片
 img = web.find_element_by_xpath("/html/body/div[3]/div/div[3]/div[1]/form/div/img").screenshot_as_png
-chaojiying = Chaojiying_Client('18371998539', '415926tX$', '951013')
+chaojiying = Chaojiying_Client('账号', '密码', '软件ID')
 dic = chaojiying.PostPic(img, 1902)
 verify_code = dic['pic_str']
 time.sleep(2)
